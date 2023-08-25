ccbf0e36ea3cc3f8775e06375641b3acaa93fb9d
Julian <mrjrhirst@gmail.com>
2023-08-25 23:59:58 +0100

    Reformatted / tidied

--- src/test/java/WeatherReporterTest.java
+++ src/test/java/WeatherReporterTest.java
@@ -6,66 +6,74 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class WeatherReporeterTester {
+public class WeatherReporterTest {
     @ParameterizedTest
-    @ValueSource(strings = { "", " ", "             "})
+    @ValueSource(strings = {"", " ", "             "})
     void testConstructorWithEmptyLocation(String location) {
         assertThrowsExactly(IllegalArgumentException.class, () -> new WeatherReporter(location, WeatherType.RAIN, 0));
     }
 
-    @Test void testConstructorWithNullLocation() {
+    @Test
+    void testConstructorWithNullLocation() {
         assertThrowsExactly(IllegalArgumentException.class, () -> new WeatherReporter(null, WeatherType.RAIN, 0));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "London", "New York", "Paris", "Munich"})
+    @ValueSource(strings = {"London", "New York", "Paris", "Munich"})
     void testGetLocationReturnsInitialisedLocation(String location) {
         WeatherReporter weather = new WeatherReporter(location, WeatherType.RAIN, 0);
         assertEquals(location, weather.getLocation());
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = { -10000.23, -1, 0, 1, 22349908.0 })
+    @ValueSource(doubles = {-10000.23, -1, 0, 1, 22349908.0})
     void testGetTemperatureReturnsInitialisedLocation(double temp) {
         WeatherReporter weather = new WeatherReporter("Testopia", WeatherType.RAIN, temp);
         assertEquals(temp, weather.getTemperature());
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "London", "New York", "Paris", "Munich"})
+    @ValueSource(strings = {"London", "New York", "Paris", "Munich"})
     void testIsLocationWhenFalse(String location) {
         WeatherReporter weather = new WeatherReporter("Testopia", WeatherType.RAIN, 0);
         assertFalse(weather.locationIs(location));
     }
+
     @ParameterizedTest
-    @ValueSource(strings = { "London", "New York", "Paris", "Munich"})
+    @ValueSource(strings = {"London", "New York", "Paris", "Munich"})
     void testIsLocationWhenTrue(String location) {
         WeatherReporter weather = new WeatherReporter(location, WeatherType.RAIN, 0);
         assertTrue(weather.locationIs(location));
     }
 
-    @Test void voidTestIconForRAIN() {
+    @Test
+    void voidTestIconForRAIN() {
         WeatherReporter weather = new WeatherReporter("Testopia", WeatherType.RAIN, 0);
-        assertEquals("☔", weather.getWeatherIcon() );
+        assertEquals("☔", weather.getWeatherIcon());
     }
 
-    @Test void voidTestIconForSNOW() {
+    @Test
+    void voidTestIconForSNOW() {
         WeatherReporter weather = new WeatherReporter("Testopia", WeatherType.SNOW, 0);
         assertEquals("⛄", weather.getWeatherIcon());
     }
 
-    @Test void voidTestIconForCLOUDY() {
+    @Test
+    void voidTestIconForCLOUDY() {
         WeatherReporter weather = new WeatherReporter("Testopia", WeatherType.CLOUDY, 0);
-        assertEquals("☁", weather.getWeatherIcon() );
+        assertEquals("☁", weather.getWeatherIcon());
     }
 
-    @Test void voidTestIconForSUNNY() {
+    @Test
+    void voidTestIconForSUNNY() {
         WeatherReporter weather = new WeatherReporter("Testopia", WeatherType.SUNNY, 0);
         assertEquals("\uD83C\uDF1E", weather.getWeatherIcon());
-    }    @Test void voidTestIconForSUNNY_INTERVALS() {
-        WeatherReporter weather = new WeatherReporter("Testopia", WeatherType.SUNNY_INTERVALS, 0);
-        assertEquals("⛅", weather.getWeatherIcon() );
     }
 
+    @Test
+    void voidTestIconForSUNNY_INTERVALS() {
+        WeatherReporter weather = new WeatherReporter("Testopia", WeatherType.SUNNY_INTERVALS, 0);
+        assertEquals("⛅", weather.getWeatherIcon());
+    }
 
 }
